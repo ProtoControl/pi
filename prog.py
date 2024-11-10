25fd3229f0b5c445f37f8537d436706d1a32b4d7
Parker Sexton <pbsexton@ncsu.edu>
2024-11-10 14:33:41 -0500

    17

--- prog.py
+++ prog.py
@@ -25,7 +25,7 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 # Define the URL
 #url = "https://protocontrol.dev/template.php"
