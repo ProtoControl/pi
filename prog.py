7c01e27db89fe63d316e3c1556de53c0438f8b45
Parker <pbsexton@ncsu.edu>
2024-11-13 14:11:02 -0500

    bcm

--- prog.py
+++ prog.py
@@ -24,7 +24,7 @@ import platform
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
 GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setmode(GPIO.BCM) # Use physical pin numbering
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 # Define the URL
