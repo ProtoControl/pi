72c90ec449b9b477dc62c0156898cfffbd7b296f
Parker <pbsexton@ncsu.edu>
2024-11-18 19:45:09 -0500

    imporr

--- prot_test.py
+++ prot_test.py
@@ -22,7 +22,7 @@ import string
 
 import platform
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-
+import serial
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
