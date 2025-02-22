028653d81b099e2206996ec83bed8b799ad739f6
AwesomeWHT <68792626+AwesomeWHT@users.noreply.github.com>
2025-02-22 12:14:48 -0500

    custom button colors

--- app/widgets/pushbuttton_widget.py
+++ app/widgets/pushbuttton_widget.py
@@ -3,11 +3,11 @@ from kivymd.uix.button import MDButton, MDButtonText
 class PushButton(MDButton):
     def __init__(self, text, id, size_hint, scolor, color, **kwargs):
         super(PushButton, self).__init__(**kwargs)
-        self.button_text = MDButtonText(text=text, text_color = scolor,theme_text_color = "Custom")
+        self.button_text = MDButtonText(text=text, text_color = color,theme_text_color = "Custom")
         self.add_widget(self.button_text)
         self.line_color = color
         #self.shadow_color = color
-        
+        self.theme_bg_color = "Custom"
         self.md_bg_color = scolor
         self.id = id
         #self.style = "filled"
